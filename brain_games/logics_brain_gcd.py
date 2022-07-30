6a9659bcd81be1ba03582a4a562a0dc614cbb678
SergeyEn <sergeyen.1995@gmail.com>
2022-07-30 19:56:39 +0300

    fix logics_brain_gcd

---
+++ brain_games/logics_brain_gcd.py
@@ -0,0 +1,35 @@
+import prompt
+from random import randint
+
+
+def search_gcd():
+    print('Welcome to the Brain Games!')
+    user_name = prompt.string('May I have your name? ')
+    print(f'Hello, {user_name}!')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    count = 0
+    gcd_result = 0
+
+    while count < 3:
+        random_num1 = randint(1, 100)
+        random_num2 = randint(1, 100)
+        print(f'Question: {random_num1} {random_num2}')
+        answer = input('Your answer: ')
+        while random_num1 != 0 and random_num2 != 0:
+            if random_num1 > random_num2:
+                random_num1 = random_num1 % random_num2
+            else:
+                random_num2 = random_num2 % random_num1
+        if random_num1 != 0:
+            gcd_result = random_num1
+        else: gcd_result = random_num2
+        if gcd_result == int(answer):
+            print('Correct!')
+        else:
+            print(f'{answer} is wrong answer ;(. Correct answer was {gcd_result}.')
+            print(f"Let's try again, {user_name}")
+            break
+
+        count += 1
+
+search_gcd()
