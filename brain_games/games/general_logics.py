34feeefb835460e829e7bd4eccfcd4eb980a0a4a
SergeyEn <sergeyen.1995@gmail.com>
2022-09-01 19:07:46 +0300

    fix general_logics

--- brain_games/games/general_logics.py
+++ brain_games/games/general_logics.py
@@ -1,7 +1,25 @@
+from random import randint
 import prompt
 
 
 def welcome_prompt():
     print('Welcome to the Brain Games!')
-    user_name = prompt.string('May I have your name? ')
-    print(f'Hello, {user_name}!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    return name
+
+def random_num():
+    random_number = randint(1, 100)
+    return random_number
+
+def victory(count, name):
+    if count == 3:
+        print(f'Congratulations, {name}!')
+
+def get_answer(answer, question, name):
+    if question == answer:
+        print('Correct!')
+    else:
+        print(f"'{answer}' is wrong answer ;(. Correct answer was '{question}'.")
+        print(f"Let's try again, {name}!")
+        return False
