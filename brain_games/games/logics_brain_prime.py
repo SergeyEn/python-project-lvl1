606b2e1a0ef5cab9005497ca0a2ef015bd192484
SergeyEn <sergeyen.1995@gmail.com>
2022-09-04 20:17:30 +0300

    fix logics_brain_prime

--- brain_games/games/logics_brain_prime.py
+++ brain_games/games/logics_brain_prime.py
@@ -1,6 +1,6 @@
-from itertools import count
-from general_logics import welcome_prompt, victory
+from general_logics import welcome_prompt, victory, get_answer
 from random import randint
+
 
 def get_count_prime(num):
     count_prime = 0
@@ -9,31 +9,25 @@ def get_count_prime(num):
             count_prime += 1
     return count_prime
 
+def get_result(num_prime):
+    if num_prime == 0:
+        return 'yes'
+    else:
+        return 'no'
+
 def log_prime():
     name = welcome_prompt()
     print("Answer \"yes\" if given number is prime. Otherwise answer \"no\".")
     count = 0
 
     while count < 3:
-        count_prime = 0
         random_num = randint(2, 100)
         print(f"Question: {random_num}")
-        get_count_prime(random_num)
+        count_prime = get_count_prime(random_num)
         answer = input("Your answer: ")
-        if count_prime == 0 and answer == "yes":
-            print('Correct!')
-        elif count_prime != 0 and answer == "no":
-            print('Correct!')
-        else:
-            if answer == 'yes':
-                right_result = 'no'
-            else:
-                right_result = 'yes'
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{right_result}'.")
-            print(f"Let's try again, {user_name}")
+        result = get_result(count_prime)
+        get_answ = get_answer(answer, result, name)
+        if get_answ == False:
             break
         count += 1
     victory(count, name)
-
-
-log_prime()
